3c4f75cbf2e1f4e234eea98160e5fb8d25713f22
unknown <ceriamalika2308@gmailcom>
2025-05-19 15:42:26 +0700

    kedua kali

---
+++ Main.m
@@ -0,0 +1,24 @@
+clc
+clear
+
+
+%Gen
+target = 'Vina_Mangedo'
+len = length(target);
+genes = create_genes(len);
+%fitnes
+fitnes = calculate_fitness(genes,len);
+%populasi
+population_size =10;
+population = create_population(target,population_size)
+%selection
+[parent1,parent2] = selection(population)
+%crossover
+[child1,child2] = crossover(parent1,parent2)
+%mutation
+mutation_rate = 0.5;
+mutant1 = mutation(child1,mutation_rate)
+mutant2 = mutation(child2,mutation_rate)
+%regeneration
+children =[mutant1,mutant2];
+population = regeneration(children,population)
